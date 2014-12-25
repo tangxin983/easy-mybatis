2c3f664f85d40673b77d89e23abac6985cfb62c4
tangxin_91 <tangxin_91@BY0301010166AX.internal.baidu.com>
2014-12-25 18:27:14 +0800

    修改注解类名

---
+++ src/main/java/com/github/tx/mybatis/annotation/ScanGeneric.java
@@ -0,0 +1,18 @@
+package com.github.tx.mybatis.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 被注解的Mapper方法，将根据泛型对象生成对应的resultMap并将泛型对象加入参数
+ * 
+ * @author tangx
+ * @since 2014年10月24日
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface ScanGeneric {
+
+}
